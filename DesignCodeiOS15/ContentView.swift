89b2f064e3551d559266dded7ae10e998e2db8aa
Vlad Ch <max.ios@test.deep.com.ua>
2023-02-04 12:49:20 +0200

    Test

--- DesignCodeiOS15/ContentView.swift
+++ DesignCodeiOS15/ContentView.swift
@@ -40,7 +40,7 @@ struct ContentView: View {
         .shadow(color: Color.ShadowColor.opacity(0.3), radius: 10, x: 0, y: 10)
         .overlay {
             RoundedRectangle(cornerRadius: 30.0, style: .continuous)
-                .stroke(.linearGradient(colors: [.white.opacity(0.2), .black.opacity(0.1)], startPoint: .topLeading, endPoint: .bottomTrailing))
+                .stroke(.linearGradient(colors: [.white.opacity(0.3), .black.opacity(0.1)], startPoint: .topLeading, endPoint: .bottomTrailing))
         }
         .padding(.horizontal, 20.0)
         .background(
