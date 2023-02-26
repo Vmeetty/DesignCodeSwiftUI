f062a8331d3fdbfccc0560583b1f71f780c0eb15
Vlad Ch <max.ios@test.deep.com.ua>
2023-02-26 17:03:57 +0200

    Avatar AsyncImage

--- AvatarView.swift
+++ AvatarView.swift
@@ -9,13 +9,26 @@ import SwiftUI
 
 struct AvatarView: View {
     var body: some View {
-        Image("Avatar Default")
-            .resizable()
-            .frame(width: 26, height: 26)
-            .cornerRadius(10)
-            .padding(8)
-            .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 18, style: .continuous))
-            .strokeStyle(cornerRadius: 18)
+        AsyncImage(url: URL(string: "https://picsum.photos/200/300"), transaction: Transaction(animation: .easeOut)) { phase in
+            
+            switch phase {
+            case .success(let image):
+                image
+                    .resizable()
+                    .transition(.scale(scale: 0.5, anchor: .center))
+            case .empty:
+                ProgressView()
+            case .failure(_):
+                Color.gray
+            @unknown default:
+                EmptyView()
+            }
+        }
+        .frame(width: 26, height: 26)
+        .cornerRadius(10)
+        .padding(8)
+        .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 18, style: .continuous))
+        .strokeStyle(cornerRadius: 18)
     }
 }
 
